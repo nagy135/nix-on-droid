28edf5a2eedaf808725d50ea929bba4cf803d301
Viktor Nagy <v.nagy@sensory-minds.com>
2026-06-13 19:45:35 +0200

    fix: which key

--- nvf/lua/which-key.lua
+++ nvf/lua/which-key.lua
@@ -1,12 +1,14 @@
 local wk = require("which-key")
 
-wk.add({
-  { "<leader>c", group = "[C]ode" },
-  { "<leader>d", group = "[D]ocument" },
-  { "<leader>r", group = "[R]ename" },
-  { "<leader>s", group = "[S]earch" },
-  { "<leader>w", group = "[W]orkspace" },
-  { "<leader>t", group = "[T]oggle" },
-  { "<leader>g", group = "[G]it", mode = { "n", "v" } },
-  { "<leader>gh", group = "Git [H]unk", mode = { "n", "v" } },
-})
+wk.register({
+  c = { name = "[C]ode" },
+  d = { name = "[D]ocument" },
+  r = { name = "[R]ename" },
+  s = { name = "[S]earch" },
+  w = { name = "[W]orkspace" },
+  t = { name = "[T]oggle" },
+  g = {
+    name = "[G]it",
+    h = { name = "Git [H]unk" },
+  },
+}, { prefix = "<leader>" })
